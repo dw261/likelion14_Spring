2be7ba19b61590cb636631e85d6d4653fc3a3f5b
dw261 <woduuc099@gmail.com>
2026-05-08 18:09:06 +0900

    6주차 과제3

--- src/main/java/com/likelion14/PBL_Spring/member/controller/HelloController.java
+++ src/main/java/com/likelion14/PBL_Spring/member/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.likelion14.PBL_Spring.member.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/hello/{name}")
+    @ResponseBody
     public String hello(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
